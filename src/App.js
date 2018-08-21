b94f1851f105a495625e707d01dad33a2a4a6c60
Erik Golden <erikgolden7@gmail.com>
2018-08-20 19:44:25 -0500

    boiler plate

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return <div className="App">Stripe Demo</div>;
+  }
+}
+
+export default App;
